8c18f070db05441b9d644e1949b73452f8604a27
Mathew Geraldo Pedro Rottye <mathewgeraldo69@gmail.com>
2024-11-22 14:21:22 +0700

    Update StudentController add index function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Imports\StudentsImport;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\StudentResource;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
@@ -14,5 +16,12 @@ class StudentController extends Controller
         Excel::import(new StudentsImport, $request->file, null, \Maatwebsite\Excel\Excel::XLSX);
 
         return 'Success!';
+    }
+
+    public function index()
+    {
+        $students = Student::all();
+
+        return StudentResource::collection($students);
     }
 }
